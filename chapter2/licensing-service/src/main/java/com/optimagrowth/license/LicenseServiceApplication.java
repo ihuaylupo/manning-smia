b61cdbcd449da5b3fd19af335e9a764b1663f806
Illary Huaylupo <illary.huaylupo@gorillalogic.com>
2020-03-04 15:56:38 -0600

    Chapter 02 source code

---
+++ chapter2/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+
+}
---
+++ chapter2/licensing-service/src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
